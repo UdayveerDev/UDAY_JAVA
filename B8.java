f034d79580fcf16ed05ac23f3927c62482d50e42
udayveer singh <udayc947@gmail.com>
2026-05-29 00:06:20 +0530

    add explanation of instance vs static blocks in Java

---
+++ B8.java
@@ -0,0 +1,12 @@
+/*
+                         Q. Difference between Instance & Static block?
+
+
+       Instance                                   |                     Static
+① It deals with Object.                           |    ① It deals with Class.
+② Executed at the time of object creation.        |   ② Executed at the time of loaded .class file in JVM.
+inside the instance block.                        |  ③ Static keyword is required.
+③ No any keyword required.                        | ④ Only static variable can be accessed inside the 
+④ Static & non-static variable can be accessed    |     static block.
+
+*/
