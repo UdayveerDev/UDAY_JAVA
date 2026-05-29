e534b317095ec7078fb4e51ebb5729d3e9de52b4
udayveer singh <udayc947@gmail.com>
2026-05-29 20:25:02 +0530

    add B17.java file with example of encapsulation in Java

---
+++ B17.java
@@ -0,0 +1,17 @@
+// JAVA  Encapsulation 
+class A{
+    private int value;// data hiding
+    public void setValue(int x){ // data abstraction 
+        value =x;
+    }
+    public int getValue(){
+        return value ;
+    }
+}
+class B{
+    public static void main (String [] args){
+        A r =new A();
+        r.setValue(100);
+        System.out.println(r.getValue);  
+    }
+}
