4302053eb2f498990f8aae1c97659ecb45b07afb
udayveer singh <udayc947@gmail.com>
2026-05-29 20:39:53 +0530

    refactor B6.java to demonstrate inheritance and method overriding; add B18.java as a new file

--- B6.java
+++ B6.java
@@ -3,13 +3,21 @@ Ans -> Static block is such kind
 of block in java which gets executed
 at the time of loading the .class file into JVM memory
 */
-class B6 {
-    public static void main (String []args){
 
+
+class A{
+    void show (){
+        System.out.println("this is the show method");
     }
-
-    // this is the static block it runs firstly then instance and then constructor 
-    static {
-        System.out.println("please help");
+}
+class  B extends A{
+    void m1(){
+        System.out.println("this is class B method");
+    }
+}
+class C {
+    public static void main (String args [] ){
+        B r=new B();
+        r.show();
     }
 }
