7b331d1e6bab5b556165ab4687fc62db203f929d
udayveer singh <udayc947@gmail.com>
2026-06-01 22:53:35 +0530

    add B41.java with documentation explaining checked and unchecked exceptions in Java

---
+++ B41.java
@@ -0,0 +1,6 @@
+/*
+Checked Exception: These exceptions are checked at compile time, forcing the programmer 
+to handle them explicitly.
+Unchecked Exception: These exceptions are checked at runtime and do not require explicit
+ handling at compile time.
+*/
