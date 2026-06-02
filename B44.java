706669c5cba7471c2419e659f04887cb6b16c602
udayveer singh <udayc947@gmail.com>
2026-06-02 10:11:16 +0530

    add B44.java with documentation explaining the use of final, finally, and finalize in Java

---
+++ B44.java
@@ -0,0 +1,22 @@
+/*
+final,finally and finalize 
+
+final is a keyword which can can be used before variable, method and class.
+for eg:- final int a=12;  // variable should be initialized with some value  
+
+
+final method :-
+final method can not be @Override 
+
+final class :-
+final class can not be extends 
+
+
+finally {}                            |                         finalize(){}
+ -> finally is a block                |  -> finalize is a method 
+ -> it is alway executed whather      | -> it is used to deallocate resources which is      
+      exception is handled or not.    |      which is allocated by unused objects
+                                      |                                                   
+                                      |
+
+*/  
