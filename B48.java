4d80fe236bb41085372d3024f5e029f4420c1092
udayveer singh <udayc947@gmail.com>
2026-06-05 22:36:08 +0530

    refactor B48.java to implement a wait method with exception handling using try-catch

--- B48.java
+++ B48.java
@@ -1,8 +1,11 @@
-class B48
+// try - catch vs thows 
+
+
+class B48 
 {
-    public static void wait()
+    public static void Wait() throws InterruptedException
     {
-        for (int i=0;i<10;i++)
+        for (int i=1;i<=10;i++)
         {
             System.out.println(i);
             Thread.sleep(1000);
@@ -12,6 +15,16 @@ class B48
 
     public static void main (String []args)
     {
-        
+        try
+        {
+            Wait();
+            System.out.println(10/0);
+
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception Handled!");
+        }
+         System.out.println("main method ended");
     }
 }
