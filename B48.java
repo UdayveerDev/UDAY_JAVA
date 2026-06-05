85d97498b7721862430340837b44c71572973f90
udayveer singh <udayc947@gmail.com>
2026-06-05 22:18:56 +0530

    add B48.java with implementation of a wait method demonstrating thread sleep functionality

---
+++ B48.java
@@ -0,0 +1,17 @@
+class B48
+{
+    public static void wait()
+    {
+        for (int i=0;i<10;i++)
+        {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+    }
+
+
+    public static void main (String []args)
+    {
+        
+    }
+}
