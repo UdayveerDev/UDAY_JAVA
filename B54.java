ae7e206f4a86014fc7c6085eccf3db16f1393cee
udayveer singh <udayc947@gmail.com>
2026-06-07 09:25:01 +0530

    add B54.java with implementation of file reading and exception handling

---
+++ B54.java
@@ -0,0 +1,28 @@
+import java.io.*;
+class B54 
+{
+    public static void main(String [] args)
+    {
+        try
+        {
+            FileReader r=new FileReader("C:\\Users\\DELL\\Desktop\\demo.txt");
+            try
+            {
+                int i;
+                while((i=r.read())!=-1)
+                {
+                    System.out.println((char)i);
+                }
+            }
+            finally
+            {
+                r.close();
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+}
