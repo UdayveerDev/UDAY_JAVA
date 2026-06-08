b634cf454ae900d9b239366ac2f5b005d856a073
udayveer singh <udayc947@gmail.com>
2026-06-08 11:40:55 +0530

    add B63.java with implementation of Runnable interface for multithreading example

---
+++ B63.java
@@ -0,0 +1,26 @@
+// BY IMPLEMENTING RUNNABLE THREAD
+class A implements Runnable 
+{
+    public void run()
+    {
+        for (int i=1;i<=5;i++)
+        {
+            System.out.println("The child Thread");
+        }
+    }
+}
+
+class B63
+{
+    public static void main(String []args)
+    {
+        A r=new A();
+        Thread t=new Thread(r);
+        t.start();
+
+        for (int i=1;i<=5;i++)
+        {
+            System.out.println("Main THread");
+        }
+    }
+}
