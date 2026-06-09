2ad71fea5c936ed2298bda86e8fc477598b5d973
udayveer singh <udayc947@gmail.com>
2026-06-09 08:49:18 +0530

    add B72.java with implementation of thread priority management using setPriority() and getPriority() methods

---
+++ B72.java
@@ -0,0 +1,37 @@
+/*
+setPriority() method is a static method which sets the priority for a thread 
+getPriority() method is a static method which gives the priority as the output 
+*/
+class A extends Thread 
+{
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getPriority());
+
+    }
+}
+
+class B72
+{
+    public static void main(String []args)
+    {
+        A t1 = new A();
+        A t2 = new A();
+        A t3 = new A();
+        
+
+        t1.setName("t1 thread");
+        t2.setName("t2 thread");
+        t3.setName("t3 thread");
+
+        t1.setPriority(2);
+        t2.setPriority(6);
+        t3.setPriority(7);
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
+}
