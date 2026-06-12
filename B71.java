f0c86bbb8490113e83e578e69faf250d564f65d3
udayveer singh <udayc947@gmail.com>
2026-06-12 21:04:13 +0530

    refactor B71.java to improve thread execution logic and enhance interrupt handling

--- B71.java
+++ B71.java
@@ -8,34 +8,33 @@ Function: It is used to interrupt a thread, signaling it to stop what it is doin
 Key Condition for Execution: The interrupt() method only works if the thread is in a 
 sleeping or waiting state (e.g., via Thread.sleep() or wait()
 */
-class A extends Thread 
+class A extends Thread  
 {
-    public void run ()
+    public void run()
     {
         try
         {
-            for (int i=1;i<=5;i++)
-            {
-                System.out.println("t1 thread running....");
-                Thread.sleep(1000);
-
-            }
-        }
-        catch(InterruptedException i)
+        for (int i=1;i<=3 ; i++)
         {
-            System.out.println("t1 thread terminated..");
+            System.out.println("Thread t1 is running....");
+            Thread.sleep(1000);
+        }
+        }
+        catch (InterruptedException i)
+        {
+            System.out.println(i);
 
         }
     }
 }
 
-class B71
+
+class B71 
 {
     public static void main (String args [])
     {
-        A t1 = new A();
+        A t1 = new A ();
         t1.start();
-
         t1.interrupt();
     }
 }
