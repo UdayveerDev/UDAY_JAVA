ee0c0d33be520886beb885eee12857d26304018e
udayveer singh <udayc947@gmail.com>
2026-06-12 20:56:05 +0530

    refactor B72.java to enhance thread naming and priority management

--- B72.java
+++ B72.java
@@ -8,30 +8,26 @@ class A extends Thread
     {
         System.out.println(Thread.currentThread().getName());
         System.out.println(Thread.currentThread().getPriority());
-
     }
 }
-
 class B72
 {
-    public static void main(String []args)
+    public static void main(String args [])
     {
-        A t1 = new A();
-        A t2 = new A();
-        A t3 = new A();
-        
+        A t1 = new A ();
+        A t2 = new A ();
+        A t3 = new A ();
 
-        t1.setName("t1 thread");
-        t2.setName("t2 thread");
-        t3.setName("t3 thread");
+        t1.setName("Thread 1");
+        t2.setName("Thread 2");
+        t3.setName("Thread 3");
 
         t1.setPriority(2);
-        t2.setPriority(6);
-        t3.setPriority(7);
+        t2.setPriority(3);
+        t3.setPriority(5);
 
         t1.start();
         t2.start();
         t3.start();
-
     }
 }
